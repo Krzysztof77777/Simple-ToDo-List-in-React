6182763f29cd257052823fb070735c24e76fa780
Krzysztof Sluzewski <krzysztofsluzewski@gmail.com>
2021-08-15 00:11:40 +0200

    feat: add main component of ToDo List App

--- src/components/TDLMain.js
+++ src/components/TDLMain.js
@@ -1,17 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TDLMain() {
+import TDLListDoneTasks from "./TDLListDoneTasks";
+
+function TDLMain({ tasks, handleCheckedTask, removeTask }) {
+  const [shownEndedTasksComponent, setShownEndedTasksComponent] =
+    useState(false);
+
+  const showEndedTasksComponent = () => {
+    if (shownEndedTasksComponent) return;
+    setShownEndedTasksComponent(true);
+  };
+  const hideEndedTasksComponent = () => {
+    if (!shownEndedTasksComponent) return;
+    setShownEndedTasksComponent(false);
+  };
+
+  const Elements = tasks
+    .filter((element) => {
+      if (!element.done) {
+        return element;
+      } else return false;
+    })
+    .map((element) => {
+      return (
+        <li key={element.id} className={element.checked ? "toright" : ""}>
+          <input
+            type="checkbox"
+            id="element"
+            checked={element.checked}
+            onChange={() => handleCheckedTask(element.id)}
+          ></input>
+          <label htmlFor="element" className="element">
+            <i className="fas fa-check"> </i>
+          </label>
+          <p>{element.name}</p>
+        </li>
+      );
+    });
+
   return (
     <div className="TDL__main">
       <div className="TDL__buttons">
-        <button className="TDL__button">
-          <i className="fas fa-list"></i>
+        <button className="TDL__button" onClick={hideEndedTasksComponent}>
+          <i
+            className={
+              !shownEndedTasksComponent ? "fas fa-list active" : "fas fa-list"
+            }
+          ></i>
         </button>
-        <button className="TDL__button">
-          <i className="far fa-calendar-check"></i>
+        <button
+          className="TDL__button TDL__button--gray"
+          onClick={showEndedTasksComponent}
+        >
+          <i
+            className={
+              shownEndedTasksComponent
+                ? "far fa-calendar-check active"
+                : "far fa-calendar-check"
+            }
+          ></i>
         </button>
       </div>
-      <div className="TDL__list"></div>
+      <div className="TDL__list list">
+        {Elements.length === 0 ? (
+          <p> Brak aktywnych tasków </p>
+        ) : (
+          <ul> {Elements} </ul>
+        )}
+        <TDLListDoneTasks
+          tasks={tasks}
+          shown={shownEndedTasksComponent}
+          removeTask={removeTask}
+          handleCheckedTask={handleCheckedTask}
+        ></TDLListDoneTasks>
+      </div>
     </div>
   );
 }
